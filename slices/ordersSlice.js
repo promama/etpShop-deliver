0927155cf1a8cd1d16ff094fb02da4aaa25cd85b
promama <promama@users.noreply.github.com>
2024-12-09 19:49:11 +0700

    Change base url

--- slices/ordersSlice.js
+++ slices/ordersSlice.js
@@ -13,9 +13,9 @@ const initialState = {
   cancel: [],
 };
 
-// const base_url = "https://e-tpshop-backend.onrender.com";
+const base_url = "https://e-tpshop-backend.onrender.com";
 // const base_url = "http://192.168.0.108:5000";
-const base_url = "http://192.168.100.23:5000";
+// const base_url = "http://192.168.100.23:5000";
 
 export const fetchFinishOrder = createAsyncThunk(
   "order/fetchFinishOrder",
